a915c9cf874f7b79a5c842ca369a9e253211e182
gursimranjot93-dev <gursimranjot93@gmail.com>
2026-03-18 00:34:26 -0700

    Add files via upload

---
+++ libraries/wood-deflection-limits.js
@@ -0,0 +1,23 @@
+window.StructuralCalcLibraries = window.StructuralCalcLibraries || {};
+window.StructuralCalcLibraries.woodDeflectionLimits = [
+  {
+    "label": "Floors",
+    "ratio": 360
+  },
+  {
+    "label": "Ceilings with brittle finishes (including plaster and stucco)",
+    "ratio": 360
+  },
+  {
+    "label": "Ceilings with flexible finishes (including gypsum board)",
+    "ratio": 240
+  },
+  {
+    "label": "All other structural members",
+    "ratio": 240
+  },
+  {
+    "label": "Lintels supporting masonry veneer walls",
+    "ratio": 600
+  }
+];
